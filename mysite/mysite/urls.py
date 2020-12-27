3d2b6377d8130b0f99a2c3b6083cc75a58429406
ilhamday <ilhamyudha00@gmail.com>
2020-12-28 05:55:18 +0700

    Update models
    - add 2 models
    - one-to-many relationship

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # The include() function allows referencing other URLconfs
     path('polls/', include('polls.urls')),
 ]
