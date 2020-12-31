3763fb5da87191f3493a82dc1e8bec3d88f3c57d
ilhamday <ilhamyudha00@gmail.com>
2021-01-01 06:07:33 +0700

    Add error exception if question not found

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -15,7 +15,13 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}.')
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Questions does not exist')
+
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = f'You\'re looking at the results of question {question_id}.'
