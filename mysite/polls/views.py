ab7d9cd02baaff2f42db4cdf4b5cb577f5836955
ilhamday <ilhamyudha00@gmail.com>
2021-01-07 22:14:57 +0700

    Testing testing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -14,13 +15,19 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        # Return the last five published questions
-        return Question.objects.order_by('-pub_date')[:5]
+        # Return the last five published questions,
+        # not including those set to be published in the fututre
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     # Using model attribute so that each generic know what model it will be acting upon
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        # Excludes any questions that aren't published yet
+        # so user can't find/guess it via urls
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
