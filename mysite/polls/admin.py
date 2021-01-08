9938d3d73b24924ff27a6fcadeae9fdc43160a3c
ilhamday <ilhamyudha00@gmail.com>
2021-01-08 09:19:41 +0700

    Edit admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+# Interface for choice in tabular style
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # reordering fields on the edit form in django admin
+    # fields = ['pub_date', 'question_text']
+
+    # split the form 
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date information', {'fields':['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
