4c97600b8169f842069a90e21f0f3fb3d1994eb9
ilhamday <ilhamyudha00@gmail.com>
2021-03-01 14:11:17 +0700

    update settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app's name -> polls, inside apps.py there are PollsConfig class
     'polls.apps.PollsConfig'
 ]
 
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
